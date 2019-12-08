2c402213302bc2e57dcaa3a2513751be02413303
Jonah <ju374@nyu.edu>
2019-12-08 00:42:23 -0600

    refactor light bulb & run

--- lib/light_bulb.py
+++ lib/light_bulb.py
@@ -1,8 +1,3 @@
-# General program setup
-from dotenv import load_dotenv
-from os import environ
-
-# Lightbulb utils
 from lifxlan import Light, WorkflowException
 import sounddevice
 import numpy
@@ -10,13 +5,6 @@ import color_converter
 
 
 class LightBulb(Light):
-    def __init__(self):
-        # General setup
-        load_dotenv()
-
-        # Bulb setup
-        super().__init__(environ['LIGHT_MAC'], environ['LIGHT_IP'])
-
     def __enter__(self):
         # Turn on bulb with 3 second transition
         self.set_power(1, 3000)
@@ -36,13 +24,14 @@ class LightBulb(Light):
         super().set_color(hsbk)
 
     def start_listening(self, duration=10):
-        super().set_color((0, 0, 0, 0))
+        super().set_color(LightBulb.hsbk_min())  # Dim bulb at start
         prev_volume = 0
 
         def sound_volume(indata, outdata, frames, time, status):
             nonlocal prev_volume
             volume = numpy.linalg.norm(indata)
             if volume > 2 and volume > 2*prev_volume:
+                # Volume is increasing, light up bulb
                 if volume < 5:
                     color = LightBulb.hsbk_max(saturation=0, brightness=15000)
                 elif volume < 10:
@@ -57,14 +46,16 @@ class LightBulb(Light):
                 except WorkFlowException as e:
                     # Occurs very occasionally when rapid is enabled
                     pass
-
             elif volume < 1 or volume < 2 and 2*volume < prev_volume:
-                self.set_color((0, 0, 0, 0), duration=10)
-
+                # Volume is declining, dim bulb
+                self.set_color(LightBulb.hsbk_min(), duration=10)
             prev_volume = volume
-
         with sounddevice.Stream(callback=sound_volume):
             sounddevice.sleep(duration * 1000)
+
+    @staticmethod
+    def hsbk_min(hue=0, saturation=0, brightness=0, kelvin=2500):
+        return (hue, saturation, brightness, kelvin)
 
     @staticmethod
     def hsbk_max(hue=65535, saturation=65535, brightness=65535, kelvin=9000):
--- lib/run.py
+++ lib/run.py
@@ -1,7 +1,13 @@
+# Utils
+from os import environ
+from dotenv import load_dotenv
+
+# Core
 from frontend.color_picker import start_frontend
 from light_bulb import LightBulb
-from os import environ
+
 
 if __name__ == '__main__':
-    with LightBulb() as my_bulb:
+    load_dotenv()
+    with LightBulb(environ['LIGHT_MAC'], environ['LIGHT_IP']) as my_bulb:
         start_frontend(my_bulb, environ['HOST'], environ['PORT'])
