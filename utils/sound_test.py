51ba28d6e757f367ce8314d6b77422f70bc74ad8
Jonah <ju374@nyu.edu>
2019-12-08 18:41:06 -0600

    moved utils docstrings to top of module

--- utils/sound_test.py
+++ utils/sound_test.py
@@ -1,12 +1,13 @@
+'''SOURCE:
+https://stackoverflow.com/questions/40138031/how-to-read-realtime-microphone-audio-volume-in-python-and-ffmpeg-or-similar
+'''
+
 import sounddevice
 import numpy
 
 
 def print_sound(indata, outdata, frames, time, status):
-    '''
-    SOURCE:
-    https://stackoverflow.com/questions/40138031/how-to-read-realtime-microphone-audio-volume-in-python-and-ffmpeg-or-similar
-    '''
+
     volume_norm = numpy.linalg.norm(indata)*10
     print("|" * int(volume_norm))
 
